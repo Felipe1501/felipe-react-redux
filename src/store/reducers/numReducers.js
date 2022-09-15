febcc8167ff8729bef0b254d838c5d42500d6289
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-15 12:04:54 -0300

    aula 170 fix: refactoring reducer

---
+++ src/store/reducers/numReducers.js
@@ -0,0 +1,23 @@
+const initialState = {
+    min: 1,
+    max: 10
+}
+
+export default function(state = initialState, action){
+        
+    switch(action.type){
+        case 'NM_MIN_CHANGE':
+            return{
+                ...state,
+                min: action.payload
+            }
+        case 'NM_MAX_CHANGE':
+            return{
+                ...state,
+                max: action.payload
+            }
+
+            default:
+                return state
+    }
+} 
--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,36 +1,7 @@
 import {createStore, combineReducers} from 'redux';
-
+import numReducers from './reducers/numReducers';
 const reducers = combineReducers({
-    numbers: function(state, action){
-        
-        switch(action.type){
-            case 'NM_MIN_CHANGE':
-                return{
-                    ...state,
-                    min: action.payload
-                }
-            case 'NM_MAX_CHANGE':
-                return{
-                    ...state,
-                    max: action.payload
-                }
-
-                default:
-                    return {
-                        min: 7,
-                        max: 31
-                    }
-        }
-    } ,
-    names: function(state, action){
-        console.log('Reducer names...');
-       console.log(state, ' ', action);
-        return[
-            'Ana',
-            'Bianca',
-            'Felipe'
-        ]
-    }
+    numbers: numReducers,
 });
 
 function storeConfig() {
