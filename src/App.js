b95ba88919edc33cd85e7ac0527e671a7ddc6605
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-15 10:04:00 -0300

    aula 158 fix: styling cards

--- src/App.js
+++ src/App.js
@@ -7,15 +7,23 @@ function App() {
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className='linha'>
-        <Card />
-        <Card />
-        <Card />
+        <Card title="Card 1" red>
+          VERMELHO
+        </Card> 
+        <Card title="Card 2" blue>
+          AZUL
+        </Card> 
+
 
       </div>
       <div className='linha'>
-        <Card />
-        <Card />
-        <Card />
+        <Card title="Card 3" green>
+         VERDE
+        </Card>
+        <Card title="Card 4" purple>
+          ROXO
+        </Card>
+
 
       </div>
     </div>
